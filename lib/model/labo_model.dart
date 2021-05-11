47b16b97307a6d7e4a8978ce5e8f13eafcc168f2
khawla <chahdoura20@gmail.com>
2021-05-11 15:47:11 +0200

    add labo

--- lib/model/labo_model.dart
+++ lib/model/labo_model.dart
@@ -2,10 +2,11 @@ class LaboModel{
   String id;
   String name;
   String image;
+  String adress;
   num rating;
   //geopoint location:
 
-  LaboModel({this.id,this.name,this.image,this.rating});
+  LaboModel({this.id,this.name,this.image,this.rating,this.adress});
 
 
 }
---
+++ lib/provider/provider_add.dart
@@ -0,0 +1,23 @@
+import 'package:best_flutter_ui_templates/model/labo_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class provider_add {
+
+  String id;
+  String name;
+  String image;
+  String adress;
+  num rating;
+
+  Future setUserData() async {
+
+    return FirebaseFirestore.instance.collection('usersData').doc().set({
+      'id':id,
+      'name':name,
+      'image': image,
+      'adress': adress,
+      'rating':rating
+    });
+  }
+}
